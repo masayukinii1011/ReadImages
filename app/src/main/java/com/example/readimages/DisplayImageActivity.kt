87f3430dc090a3fa9a8762101af6913da195c9b0
masayukinii1011 <masayukinii1011@gmail.com>
2020-05-03 19:38:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/readimages/DisplayImageActivity.kt
@@ -0,0 +1,18 @@
+package com.example.readimages
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_display_image.*
+
+class DisplayImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_image)
+
+        var path = intent.getStringExtra("path")
+        var bitmap = BitmapFactory.decodeFile(path)
+        imageView.setImageBitmap(bitmap)
+    }
+}
